77e8ec81eed9348b60bbabce4ad93b596d3312d9
agth <aagetenhengel@gmail.com>
2021-01-22 14:43:37 +0100

    Day 1 part 2 answer

--- AdventOfCode2020/Functions/Day1.fs
+++ AdventOfCode2020/Functions/Day1.fs
@@ -8,8 +8,23 @@ module Day1
                   if x + y = 2020 then yield (x, y)
         }
 
+    let tripleThatSumTo2020 (inputs: seq<int>) =
+
+        seq {
+                for x in inputs do
+                 for y in inputs do
+                  for z in inputs do
+                   if x + y + z = 2020 then yield (x, y, z)
+        }
+
     let one inputs =
 
         setThatSumTo2020 inputs
         |> Seq.map (fun (x, y) -> x * y)
+        |> Seq.head
+
+    let two inputs =
+
+        tripleThatSumTo2020 inputs
+        |> Seq.map (fun (x, y, z) -> x * y * z)
         |> Seq.head
--- AdventOfCode2020/Program.fs
+++ AdventOfCode2020/Program.fs
@@ -7,7 +7,7 @@ open Day1
 [<EntryPoint>]
 let main argv =
     let inputs = readPuzzleInput 1 One
-    let answer = inputs |> Seq.map int |> one
+    let answer = inputs |> Seq.map int |> two
 
     printfn "%i" answer
     0 // return an integer exit code
