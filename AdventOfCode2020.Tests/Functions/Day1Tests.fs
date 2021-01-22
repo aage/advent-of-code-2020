4c3178695fe8830ca3ecf4f47c94c6012f8633e9
agth <aagetenhengel@gmail.com>
2021-01-22 14:46:36 +0100

    Add intg test day 1 part 2

--- AdventOfCode2020.Tests/Functions/Day1Tests.fs
+++ AdventOfCode2020.Tests/Functions/Day1Tests.fs
@@ -11,3 +11,9 @@ let ``day 1 part 1`` () =
     let actual = one inputs
     Assert.Equal(expected, actual)
 
+[<Fact>]
+let ``day 1 part 2`` () =
+    let expected = 281473080
+    let inputs = readPuzzleInput 1 One |> Seq.map int
+    let actual = two inputs
+    Assert.Equal(expected, actual)
