b9117a96b134c65b1a89472de855c25b2e2e02c6
agth <aagetenhengel@gmail.com>
2021-01-23 08:28:28 +0100

    Simplify day 1 part 2 by inlining specific sequence instead of in a local function

--- AdventOfCode2020/Functions/Day1.fs
+++ AdventOfCode2020/Functions/Day1.fs
@@ -2,15 +2,6 @@ module Day1
 
     open Utilities
 
-    let tripleThatSumTo2020 (inputs: seq<int>) =
-
-        seq {
-                for x in inputs do
-                 for y in inputs do
-                  for z in inputs do
-                   if x + y + z = 2020 then yield (x, y, z)
-        }
-
     let one inputs =
 
         combinations inputs
@@ -20,6 +11,10 @@ module Day1
 
     let two inputs =
 
-        tripleThatSumTo2020 inputs
-        |> Seq.map (fun (x, y, z) -> x * y * z)
-        |> Seq.head
+        let nums =
+            seq {
+             for (x,y) in combinations inputs do
+              for z in inputs do
+               if x + y + z = 2020 then yield x * y * z }
+
+        Seq.head nums
