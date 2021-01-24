6bd940bfe4eb38c8090b5fa1be511f9d3ebf9b77
agth <aagetenhengel@gmail.com>
2021-01-24 10:06:22 +0100

    Some formatting

--- AdventOfCode2020/Functions/Day2.fs
+++ AdventOfCode2020/Functions/Day2.fs
@@ -1,16 +1,16 @@
 module Day2
 
     let applyPasswordPolicy  (data:string) =
-        let parts = data.Split(' ') // data looks lieke"1-3 b: cdefg"
+        let parts = data.Split(' ') // data looks like "1-3 b: cdefg"
         let range = parts.[0].Split('-') |> Array.map int
         let letter = parts.[1].[0]
         let pass = parts.[2]
-        let cs =
+        let occurences =
             pass
             |> String.filter (fun c -> c = letter)
             |> String.length
 
-        if [range.[0] .. range.[1]] |> List.contains cs
+        if [range.[0] .. range.[1]] |> List.contains occurences
         then Ok pass
         else Error "Password not valid"
 
