859187b3d0b78af10dd5de1a8beaebfc82f6c249
agth <aagetenhengel@gmail.com>
2021-01-24 15:01:19 +0100

    Add position password policy fun for day 2 part 2

--- AdventOfCode2020.Tests/Functions/Day2Tests.fs
+++ AdventOfCode2020.Tests/Functions/Day2Tests.fs
@@ -23,6 +23,23 @@ let ``password policy occurences correctly determines an invalid password`` () =
     Assert.Equal(expected,actual)
 
 [<Fact>]
+let ``password policy position correctly determines a valid password`` () =
+    let data = "1-3 a: abcde"
+    let expected = Ok "abcde"
+
+    let actual = passwordPolicyPosition data
+
+    Assert.Equal(expected,actual)
+
+[<Theory>]
+[<InlineData("1-3 b: cdefg")>]
+[<InlineData("2-9 c: ccccccccc")>]
+let ``password policy position correctly determines an invalid password`` data =
+    let expected = Error "Password not valid"
+    let actual = passwordPolicyPosition data
+    Assert.Equal(expected,actual)
+
+[<Fact>]
 let ``day 2 part 1`` () =
     let expected = 622
     let inputs = readPuzzleInput 2
--- AdventOfCode2020/Functions/Day2.fs
+++ AdventOfCode2020/Functions/Day2.fs
@@ -14,6 +14,21 @@ module Day2
         then Ok pass
         else Error "Password not valid"
 
+    let passwordPolicyPosition (data:string) =
+
+        let parts = data.Split(' ') // data looks like "1-3 b: cdefg"
+        let positions =
+            parts.[0].Split('-')
+            |> Array.map (fun c -> (int c) - 1)
+        let letter = parts.[1].[0]
+        let pass = parts.[2]
+
+        let letters = (pass.[positions.[0]], pass.[positions.[1]])
+
+        if (fst letters = letter) <> (snd letters = letter)
+        then Ok pass
+        else Error "Password not valid"
+
     let one inputs =
 
         inputs
