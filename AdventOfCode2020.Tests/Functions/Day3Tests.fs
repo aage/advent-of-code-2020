2f9c10f5e17573554a62f2102543d41f187f77ef
agth <aagetenhengel@gmail.com>
2021-01-30 07:11:37 +0100

    Day 3 part 1 answer

--- AdventOfCode2020.Tests/Functions/Day3Tests.fs
+++ AdventOfCode2020.Tests/Functions/Day3Tests.fs
@@ -2,6 +2,7 @@ module Day3Tests
 
 open Day3
 open Xunit
+open FileSystem
 
 [<Fact>]
 let ``toboggan correctly counts amount of trees`` () =
@@ -22,3 +23,12 @@ let ``toboggan correctly counts amount of trees`` () =
     let actual = one data
     
     Assert.Equal(expected,actual)
+
+[<Fact>]
+let ``day 2 part 1`` () =
+    let expected = 276
+    let inputs = readPuzzleInput 3
+
+    let actual = one inputs
+
+    Assert.Equal(expected, actual)
