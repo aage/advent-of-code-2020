1af7d0cace949d0294c9fa746da4ee9f4d3fb38d
agth <aagetenhengel@gmail.com>
2021-05-07 10:31:50 +0200

    Add day 7 function to determine how many bags can hold initial bag, directly or indirectly

--- AdventOfCode2020.Tests/Functions/Day7Tests.fs
+++ AdventOfCode2020.Tests/Functions/Day7Tests.fs
@@ -3,6 +3,19 @@ module Day7Tests
     open Xunit
     open Day7
 
+    let rules =
+        [
+            "light red bags contain 1 bright white bag, 2 muted yellow bags."
+            "dark orange bags contain 3 bright white bags, 4 muted yellow bags."
+            "bright white bags contain 1 shiny gold bag."
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags."
+            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags."
+            "dark olive bags contain 3 faded blue bags, 4 dotted black bags."
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags."
+            "faded blue bags contain no other bags."
+            "dotted black bags contain no other bags."
+        ]
+
     [<Theory>]
     [<InlineData(
         "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
@@ -35,3 +48,12 @@ module Day7Tests
 
         Assert.Equal(expected,actual)
 
+    [<Fact>]
+    let ``a set of rules determines how many bags can hold a specific bag`` () =
+
+        let expected = 4
+        let bag = { Color = Color "shiny gold" ; CanContain = [] }
+
+        let actual = bagsThatCanHoldBag rules bag |> List.length
+
+        Assert.Equal (expected, actual)
--- AdventOfCode2020/Functions/Day7.fs
+++ AdventOfCode2020/Functions/Day7.fs
@@ -29,3 +29,39 @@ module Day7
                     (num, { Color = Color clr ; CanContain = [] }))
                 |> List.ofArray
         { Color = color ; CanContain = bags }
+
+    let bagsThatCanHoldBag (rules: string list) (bag: Bag) =
+    
+        let bags = rules |> List.map parse
+
+        let rec inner
+            (bagsToCheck: Bag list)
+            (bagsThatCanHold: Bag list)
+            (addedAny : bool) =
+
+            match (addedAny, bagsToCheck) with
+            | (false, _) -> bagsThatCanHold
+            | (true, []) -> bagsThatCanHold
+            | _  ->
+                // prefix initial bag
+                let colors =
+                    bag::bagsThatCanHold
+                    |> List.map (fun b -> b.Color)
+                    |> Set.ofList
+                // check if the colors of the bags to check overlap
+                // with the colors of the bags should be contained
+                let canHold =
+                    bagsToCheck
+                    |> List.filter (fun b ->
+                        b.CanContain
+                        |> List.map snd
+                        |> List.map (fun b -> b.Color)
+                        |> Set.ofList
+                        |> Set.intersect colors
+                        |> Seq.length > 0)
+                inner
+                    (bagsToCheck |> List.except canHold)
+                    (canHold @ bagsThatCanHold)
+                    (canHold.Length > 0)
+
+        inner bags [] true
